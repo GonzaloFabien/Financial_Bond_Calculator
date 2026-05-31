ac7b01ae57dec5b9a9ed811a55c51568763a5487
laptop <garaoz@unsa.edu.pe>
2026-05-30 20:42:51 -0500

    añadimos el segundo input text, sin vincular con la función:

--- finanzas_interfaz.py
+++ finanzas_interfaz.py
@@ -18,17 +18,29 @@ texto_inicial.pack(pady=5)
 
 #3-Se crea el input_text,:
 #7- Le añadimos estilo a este formulario:
+#4-Se añade texto por defecto en el input:
 contenedor_form = tk.Frame(ventana)
 contenedor_form.pack(pady=10)
 
-texto_izquierda = tk.Label(contenedor_form, text="Face Value:")
+texto_izquierda = tk.Label(contenedor_form, text="F= Face Value:")
 texto_izquierda.pack(side="left",padx=10)
 
 caja_texto = tk.Entry(contenedor_form, width=20)
-caja_texto.pack(pady=50)
+caja_texto.pack(pady=20)
 
-#4-Se añade texto por defecto en el input:
 caja_texto.insert(0,"100")
+
+#8- Se hace los demás formularios para cada item de los bonos:"M= Años por vencer:"
+
+contenedor_M = tk.Frame(ventana)
+contenedor_M.pack(pady=10)
+texto_izq_M = tk.Label(contenedor_M, text="M= Años por vencer:")
+texto_izq_M.pack(side="left", padx=10)
+
+caja_entry_M = tk.Entry(contenedor_M,width=20)
+caja_entry_M.pack(pady=20)
+
+
 
 #5-Se crea el boton para capturar la función:
 botoncito = tk.Button(ventana, text="Ejecutar", command=guardar_valor)
